6e453598e2324499f5a6484583e76aaa370e6e8f
이름 <ktg6361@gmail.com>
2021-09-01 16:01:03 +0900

    Add:초기세팅

--- public/data/data.js
+++ public/data/data.js
@@ -1,1 +1,55 @@
-const Data = [ { text : "시그니처 더 블랙 콜드 브루", img : "https://image.istarbucks.co.kr/upload/store/skuimg/2021/08/[9200000003661]_20210819094346176.jpg" }, { text : "아이스크림 블렌딩 콜드 브루", img : "https://image.istarbucks.co.kr/upload/store/skuimg/2021/06/[9200000003643]_20210623101238720.jpg" }, { text : "나이트로 바닐라 크림", img : "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000002487]_20210426091745467.jpg" }, { text : "나이트로 콜드 브루", img : "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000479]_20210426091843897.jpg" }, { text : "돌체 콜드 브루", img : "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000002081]_20210415133656839.jpg" }, { text : "바닐라 크림 콜드 브루", img : "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000487]_20210430112319040.jpg" }, { text : "벨벳 다크 모카 나이트로", img : "https://image.istarbucks.co.kr/upload/store/skuimg/2021/03/[9200000003509]_20210322093452399.jpg" }, { text : "제주 비자림 콜드 브루", img : "https://image.istarbucks.co.kr/upload/store/skuimg/2020/09/[9200000002672]_20200921171223898.jpg" }, { text : "콜드 브루", img : "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000038]_20210430113202458.jpg" }, { text : "콜드 브루 몰트", img : "https://image.istarbucks.co.kr/upload/store/skuimg/2021/02/[9200000001636]_20210225093600536.jpg" }, { text : "콜드 브루 오트 라떼", img : "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000003285]_20210416154437069.jpg" }, { text : "콜드 브루 플로트", img : "https://image.istarbucks.co.kr/upload/store/skuimg/2021/02/[9200000001635]_20210225092236748.jpg" }, { text : "프렌치 애플 타르트 나이트로", img : "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000003285]_20210416154437069.jpg" }, ];
+// eslint-disable-next-line no-unused-vars
+const Data = [
+  {
+    text: "시그니처 더 블랙 콜드 브루",
+    img: "https://image.istarbucks.co.kr/upload/store/skuimg/2021/08/[9200000003661]_20210819094346176.jpg",
+  },
+  {
+    text: "아이스크림 블렌딩 콜드 브루",
+    img: "https://image.istarbucks.co.kr/upload/store/skuimg/2021/06/[9200000003643]_20210623101238720.jpg",
+  },
+  {
+    text: "나이트로 바닐라 크림",
+    img: "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000002487]_20210426091745467.jpg",
+  },
+  {
+    text: "나이트로 콜드 브루",
+    img: "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000479]_20210426091843897.jpg",
+  },
+  {
+    text: "돌체 콜드 브루",
+    img: "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000002081]_20210415133656839.jpg",
+  },
+  {
+    text: "바닐라 크림 콜드 브루",
+    img: "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000487]_20210430112319040.jpg",
+  },
+  {
+    text: "벨벳 다크 모카 나이트로",
+    img: "https://image.istarbucks.co.kr/upload/store/skuimg/2021/03/[9200000003509]_20210322093452399.jpg",
+  },
+  {
+    text: "제주 비자림 콜드 브루",
+    img: "https://image.istarbucks.co.kr/upload/store/skuimg/2020/09/[9200000002672]_20200921171223898.jpg",
+  },
+  {
+    text: "콜드 브루",
+    img: "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000038]_20210430113202458.jpg",
+  },
+  {
+    text: "콜드 브루 몰트",
+    img: "https://image.istarbucks.co.kr/upload/store/skuimg/2021/02/[9200000001636]_20210225093600536.jpg",
+  },
+  {
+    text: "콜드 브루 오트 라떼",
+    img: "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000003285]_20210416154437069.jpg",
+  },
+  {
+    text: "콜드 브루 플로트",
+    img: "https://image.istarbucks.co.kr/upload/store/skuimg/2021/02/[9200000001635]_20210225092236748.jpg",
+  },
+  {
+    text: "프렌치 애플 타르트 나이트로",
+    img: "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000003285]_20210416154437069.jpg",
+  },
+];
