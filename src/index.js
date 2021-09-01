04884a9ade5426a688a8ba74238e8dd473d80f02
이름 <ktg6361@gmail.com>
2021-09-01 17:06:33 +0900

    Add: first commit. 초기 세팅 완료.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/reset.scss';
 import Routes from './Routes';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
