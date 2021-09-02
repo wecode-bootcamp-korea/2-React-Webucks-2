0ee1b1b0bfa11b46ac25757274892d4b49f65a2f
fireking5997@gmail.com <fireking5997@gmail.com>
2021-09-03 00:33:17 +0900

    fix : detail login 함수 완성

--- src/pages/pakjonghun/CommonComponents/Input/Input.js
+++ src/pages/pakjonghun/CommonComponents/Input/Input.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './Input.scss';
 
-function Input({ isValid, rref, set, value, ...rest }) {
-  return (
-    <div className="Input">
-      <input
-        className={isValid ? 'green' : null}
-        value={value}
-        ref={rref}
-        {...rest}
-        onChange={e => {
-          set(e.target.value);
-        }}
-      />
-    </div>
-  );
+//클레스에서 함수 처럼 특정 prop을 무시하는 방법 없을까?  무슨 타입 스크립도 아니고... ㅠ에러 게속뜨네. 작동은 하니 뭐... ㅠ
+class Input extends React.Component {
+  render() {
+    return (
+      <div className="Input">
+        <input
+          className={this.props.isValid ? 'green' : null}
+          value={this.props.value}
+          ref={this.props.rref}
+          {...this.props}
+          onChange={e => {
+            this.props.set(e.target.value);
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default Input;
